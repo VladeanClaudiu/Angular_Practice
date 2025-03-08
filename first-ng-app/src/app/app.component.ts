0e24110a670e78acb0a896a145ab4c0e020015cb
Claudiu Vladean <35781140+VladeanClaudiu@users.noreply.github.com>
2025-03-08 12:07:27 +0000

    proj initi

---
+++ first-ng-app/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  template: `
+    <h1>Welcome to {{title}}!</h1>
+    <p>Adding some paragrahs to the template</p>
+
+    <router-outlet />
+  `,
+  styles: [`
+    p {
+      background-color: red;
+    }`],
+})
+export class AppComponent {
+  title = 'first-ng-app';
+}
